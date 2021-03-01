6257fea9a4bccb3ab441b11ebecc2b3ca58a8339
edmilson-dk <droidvidaboa@gmail.com>
2021-03-01 16:29:40 -0300

    feat: finished view audio module

--- src/view.mjs
+++ src/view.mjs
@@ -8,10 +8,37 @@ export default class View {
   onRecordClick(command) {
     return () => {
       command();
+      this.toggleAudioVisible({ visible: false });
+    }
+  }
+
+  onStopRecordingClick(command) {
+    return () => {
+      command();
     }
   }
 
   configureStartRecordingButton(command) {
     this.btnStartElement.addEventListener('click', this.onRecordClick(command));
+  }
+
+  configureStopRecordingButton(command) {
+    this.btnStopElement.addEventListener('click', this.onStopRecordingClick(command));
+  }
+
+  toggleAudioVisible({ visible }) {
+    const classList = this.audioElement.classList;
+    visible 
+      ? classList.remove('hidden')
+      : classList.add('hidden');
+  }
+
+  playAudio(url) {
+    const audio = this.audioElement;
+
+    audio.src = url;
+    audio.muted = false;
+    this.toggleAudioVisible({ visible: true });
+    audio.addEventListener('loadedmetadata', _ => audio.play());
   }
 }
